05d2ff116d371f4aa633b396b48d3ddd0e8a6c83
unknown <nastyakudashevaaface@gmail.com>
2017-11-23 23:09:12 +0600

    Учебный проект: Холст

--- js/stat.js
+++ js/stat.js
@@ -1,53 +1,61 @@
-window.renderStatistics = function(ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
+  var drawRect = function (x, y, width, height, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height);
+  };
 
-		ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-		ctx.fillRect(110, 20, 420, 270);
+  drawRect(110, 20, 420, 270, 'rgba(0, 0, 0, 0.7)');
 
-		ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-		ctx.fillRect(100, 10, 420, 270);
+  drawRect(100, 10, 420, 270, 'rgba(255, 255, 255, 1)');
 
-		ctx.fillStyle = '#000';
-	  	ctx.font = '16px PT Mono';
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
 
-	  	ctx.fillText('Ура вы победили!', 120, 40);
-	  	ctx.fillText('Список результатов:', 120, 60);
+  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillText('Список результатов:', 120, 60);
 
 
-	  var max = -1;
-	  var maxIndex = -1;
+  var max;
+  max = -1;
 
-	  for (var i = 0 ; i < times.length; i++) {
-	    var time = times[i];
-	    if (time > max) {
-	      max = time;
-	      maxIndex = i;
-	    }
-	  }
-	  
-	  
-	  var histogramHeight = 150;
-	  var step = histogramHeight / (max - 0);
+  var getMaxElement = function (array) {
+    for (var i = 0; i < array.length; i++) {
+      var time = array[i];
+      if (time > max) {
+        max = time;
+      }
+    }
+  };
 
-	  var barWidth = 40; // px; 
-	  var indent = 90;    // px;
-	  var initialX = 120; // px;
-	  var initialY = 150;  // px;
-	  var lineHeight = 16;// px;
+  getMaxElement(times);
 
-	  for (var i = 0 ; i < times.length; i++) {
-	  	if (i < 1) {
-	  	ctx.fillStyle = '#FC0D1B';
-	  	} else {
-	  	ctx.fillStyle = '#020E86'; 
-	  	ctx.globalAlpha = Math.random(0, 1); 
-	  	}
+  var histogramHeight = 150;
+  var step = histogramHeight / (max - 0);
 
-	    ctx.fillRect(initialX + indent * i, 260 - times[i] * step - 10, barWidth, times[i] * step);
+  var barWidth = 40; // px;
+  var indent = 90; // px;
+  var initialX = 120; // px;
 
-	  	ctx.fillStyle = '#000';
-    	ctx.fillText(names[i], initialX + indent * i, 270);
+  var drawHistogramRect = function (x, y, width, height) {
+    ctx.fillRect(x, y, width, height);
+  };
 
-    	ctx.fillStyle = '#000';
-    	ctx.fillText(Math.floor(times[i]), initialX + indent * i, 260 - times[i] * step - 20);
-	  }
-	}
+  var drawText = function (array, x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillText(array, x, y);
+  };
+  for (var i = 0; i < times.length; i++) {
+    if (i < 1) {
+      ctx.fillStyle = '#FC0D1B';
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, 0, Math.random(1, 0))';
+    }
+
+    drawHistogramRect(initialX + indent * i, 260 - times[i] * step - 10, barWidth, times[i] * step);
+
+    drawText(names[i], initialX + indent * i, 270, '#000');
+
+    drawText(Math.floor(times[i]), initialX + indent * i, 260 - times[i] * step - 20, '#000');
+  }
+};
+
