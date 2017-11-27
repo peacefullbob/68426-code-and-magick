72645a51de82b13396b1f91dd4365dcbf34881dd
unknown <nastyakudashevaaface@gmail.com>
2017-11-27 21:14:57 +0600

    Учебный проект: Холст

--- js/stat.js
+++ js/stat.js
@@ -1,64 +1,51 @@
 'use strict';
 
+var HISTOGRAM_HEIGHT = 150;
+var BAR_WIDTH = 40;
+var INDENT = 90;
+var INITIAL_X = 120;
+
 window.renderStatistics = function (ctx, names, times) {
   var drawRect = function (x, y, width, height, color) {
     ctx.fillStyle = color;
     ctx.fillRect(x, y, width, height);
   };
 
+  var drawText = function (contentText, x, y, color, fontStyle) {
+    ctx.fillStyle = color;
+    ctx.font = fontStyle;
+    ctx.fillText(contentText, x, y);
+  };
+
   drawRect(110, 20, 420, 270, 'rgba(0, 0, 0, 0.7)');
-
   drawRect(100, 10, 420, 270, 'rgba(255, 255, 255, 1)');
-
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-
-  ctx.fillText('Ура вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 60);
-
-
-  var max;
-  max = -1;
+  drawText('Ура вы победили!', 120, 40, '#000', '16px PT Mono');
+  drawText('Список результатов:', 120, 60, '#000', '16px PT Mono');
 
   var getMaxElement = function (array) {
+    var max = array[0];
     for (var i = 0; i < array.length; i++) {
       var time = array[i];
       if (time > max) {
         max = time;
       }
     }
+    return (max);
   };
 
-  getMaxElement(times);
+  var max = getMaxElement(times);
+  var STEP = HISTOGRAM_HEIGHT / max;
 
-  var histogramHeight = 150;
-  var step = histogramHeight / (max - 0);
-
-  var BAR_WIDTH = 40;
-  var INDENT = 90;
-  var INITIAL_X = 120;
-
-  var drawHistogramRect = function (x, y, width, height) {
-    ctx.fillRect(x, y, width, height);
-  };
-
-  var drawText = function (array, x, y, color) {
-    ctx.fillStyle = color;
-    ctx.fillText(array, x, y);
-  };
-  var randomOpacity = Math.random(1, 0);
   for (var i = 0; i < times.length; i++) {
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = 'rgba(2, 14, 134,' + randomOpacity + ')';
+      ctx.fillStyle = 'rgba(2, 14, 134,' + Math.random(1, 0) + ')';
     }
 
-    drawHistogramRect(INITIAL_X + INDENT * i, 260 - times[i] * step - 10, BAR_WIDTH, times[i] * step);
-
+    drawRect(INITIAL_X + INDENT * i, 260 - times[i] * STEP - 10, BAR_WIDTH, times[i] * STEP);
     drawText(names[i], INITIAL_X + INDENT * i, 270, '#000');
-
-    drawText(Math.floor(times[i]), INITIAL_X + INDENT * i, 260 - times[i] * step - 20, '#000');
+    drawText(Math.floor(times[i]), INITIAL_X + INDENT * i, 260 - times[i] * STEP - 20, '#000');
   }
 };
 
