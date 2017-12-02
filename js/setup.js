229e92fd5e1ed339b120aa156b2d5ad8008ef45a
unknown <nastyakudashevaaface@gmail.com>
2017-12-02 19:44:06 +0600

    Учебный проект: нас орда

--- js/setup.js
+++ js/setup.js
@@ -8,48 +8,33 @@ var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Валь
 var coatsColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)]'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
-function getRandom(array) {
-  var rand = Math.floor(Math.random() * array.length);
-  return rand;
+function getRandom(number) {
+  var randNumber = Math.floor(Math.random() * number);
+  return randNumber;
 }
 
-var wizards = [
-  {
-    name: surnames[getRandom(surnames)] + ' ' + names[getRandom(names)],
-    coatColor: coatsColors[getRandom(coatsColors)],
-    eyesColor: eyesColors[getRandom(eyesColors)]
-  },
-  {
-    name: surnames[getRandom(surnames)] + ' ' + names[getRandom(names)],
-    coatColor: coatsColors[getRandom(coatsColors)],
-    eyesColor: eyesColors[getRandom(eyesColors)]
-  },
-  {
-    name: surnames[getRandom(surnames)] + ' ' + names[getRandom(names)],
-    coatColor: coatsColors[getRandom(coatsColors)],
-    eyesColor: eyesColors[getRandom(eyesColors)]
-  },
-  {
-    name: surnames[getRandom(surnames)] + ' ' + names[getRandom(names)],
-    coatColor: coatsColors[getRandom(coatsColors)],
-    eyesColor: eyesColors[getRandom(eyesColors)]
-  }
-];
+var wizards = [];
 
+for (var i = 0; i < 3; i++) {
+  var wizard = {
+    name: surnames[getRandom(surnames.length)] + ' ' + names[getRandom(names.length)],
+    coatColor: coatsColors[getRandom(coatsColors.length)],
+    eyesColor: eyesColors[getRandom(eyesColors.length)]
+  };
+  wizards[i] = wizard;
+  wizards.push(wizards[i]);
+}
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-
-
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
-
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
-
   return wizardElement;
 };
+
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < wizards.length; i++) {
   fragment.appendChild(renderWizard(wizards[i]));
